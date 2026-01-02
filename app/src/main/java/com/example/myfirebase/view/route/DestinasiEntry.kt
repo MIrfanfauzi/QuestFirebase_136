356c1e5601b7a22193875e55d2a41609e93b42dc
Irfan Fauzi <irfanfauziyk@gmail.com>
2026-01-02 15:27:47 +0700

    Menambahkan DestinasiEntry

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
