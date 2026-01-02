0947d2037ecfe9ff244d90c88dc77d9e764ecc74
Irfan Fauzi <irfanfauziyk@gmail.com>
2026-01-02 15:41:41 +0700

    Membuat StatusUiSiswa

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
