d2c9ec8c8e1a9597b6bf626747f38385521ca6be
Irfan Fauzi <irfanfauziyk@gmail.com>
2026-01-02 15:45:37 +0700

    Membuat objek PenyediaViewModel

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -9,5 +9,10 @@ import com.example.myfirebase.repositori.AplikasiDataSiswa
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
-
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
 
