1f3287f1be3b50f95c8b3612479997312205700f
Irfan Fauzi <irfanfauziyk@gmail.com>
2026-01-08 16:22:18 +0700

    Menambahkan composable untuk DestinasiEdit

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -60,5 +60,22 @@ fun HostNavigasi(
                 }
             )
         }
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiEdit.itemIdArg) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            EditSiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                },
+                onNavigateUp = {
+                    navController.navigateUp()
+                }
+            )
+        }
     }
 }
